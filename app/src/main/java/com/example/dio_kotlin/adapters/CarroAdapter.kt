3f7124dc4e7d697f8d7a4a7e652f6f44b4d6885a
matheus <matheus@novetech.com.br>
2024-05-10 22:04:23 -0300

    1.  RecycleView com Adapter co pleto

--- app/src/main/java/com/example/dio_kotlin/adapters/CarroAdapter.kt
+++ app/src/main/java/com/example/dio_kotlin/adapters/CarroAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.dio_kotlin.adapters
 
+import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
@@ -8,19 +9,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.dio_kotlin.R
 import com.example.dio_kotlin.models.Carro
 
-class CarroAdapter(private val carros: List<Carro>) :
-        RecyclerView.Adapter<CarroAdapter.ViewHolder>(){
-
-
+class CarroAdapter(private val carros: List<Carro>) : RecyclerView.Adapter<CarroAdapter.ViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.carro_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         holder.tv_potencia.text = carros[position].potencia
+        holder.tv_bateria.text = carros[position].bateria
+        holder.tv_marca.text = carros[position].marca
 
     }
 
@@ -30,11 +31,14 @@ class CarroAdapter(private val carros: List<Carro>) :
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
         val tv_potencia : TextView
+        val tv_bateria : TextView
+        val tv_marca : TextView
 
         init {
             view.apply {
-                tv_potencia = findViewById(R.id.tv_tv_potencia)
-
+                tv_potencia = findViewById(R.id.tv_tv_potencia_value)
+                tv_marca = findViewById(R.id.tv_marca_value)
+                tv_bateria = findViewById(R.id.tv_bateria_value)
             }
         }
     }
--- app/src/main/java/com/example/dio_kotlin/pages/RecycleViewActivity.kt
+++ app/src/main/java/com/example/dio_kotlin/pages/RecycleViewActivity.kt
@@ -2,11 +2,55 @@ package com.example.dio_kotlin.pages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dio_kotlin.R
+import com.example.dio_kotlin.adapters.CarroAdapter
+import com.example.dio_kotlin.models.Carro
 
 class RecycleViewActivity : AppCompatActivity() {
+
+   lateinit var rv_carros : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycle_view)
+
+        initComponents()
+        initRecyclerView()
+    }
+
+    fun initComponents(){
+        rv_carros = findViewById(R.id.rv_carros)
+    }
+    fun initRecyclerView(){
+
+        val carros = listOf(
+            Carro(
+                id = 1,
+                potencia = "300Kw",
+                marca = "VOlkswagen",
+                bateria = "3000ha",
+                urlPhoto = "drawable"
+            ),
+            Carro(
+                id = 2,
+                potencia = "800Kw",
+                marca = "Tesla",
+                bateria = "30000ha",
+                urlPhoto = "drawable"
+            ),
+            Carro(
+                id = 3,
+                potencia = "1000Kw",
+                marca = "Civic",
+                bateria = "3100ha",
+                urlPhoto = "drawable"
+            )
+
+        )
+        val adapter = CarroAdapter(carros)
+        rv_carros.adapter = adapter
+
     }
 }
